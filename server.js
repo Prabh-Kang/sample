6b7ea40c7ff3b0acd37173b42a8823b52401d614
Prabh Kang <63305822+Prabh-Kang@users.noreply.github.com>
2023-07-30 05:50:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log('Server is listening at PORT: ', process.env.PORT)
+app.listen(process.env.SAMPLE_PROJECT_PORT, () => {
+  console.log('Server is listening at PORT: ', process.env.SAMPLE_PROJECT_PORT)
 })
